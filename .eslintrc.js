57861e6975551875884c5a7168d977d34da1cc0f
Drew <bindernews@gmail.com>
2018-03-22 16:39:34 -0400

    Update eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,6 @@ module.exports = {
         "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
         "react/prefer-es6-class": [1, 1],
         "react/prefer-stateless-function": [0],
+        "linebreak-style": [0],
     }
 };
